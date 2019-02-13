76b81c5aeefa37d3a18a2b00192b73565828777d
Brent Engelbrecht <brent.engelbrecht@jembi.org>
2019-02-13 21:45:48 +0200

    Provision, build, install and teardown servers for perf testing
    OHM-755

--- do-performance-test.sh
+++ do-performance-test.sh
@@ -1,3 +1,14 @@
-ansible-playbook playbooks/do-create-servers.yml
-ansible-playbook -i inventory playbooks/deploy.yml
-ansible-playbook playbooks/do-shutdown-servers.yml
+INVNAME=`pwd`/new_inventory.ini
+
+rm -f $INVNAME
+
+ansible-playbook --extra-vars "new_inventory=$INVNAME" playbooks/do-create-servers.yml
+
+if [[ ! -f "$INVNAME" ]]; then
+  echo "Error: No inventory file at $INVNAME after droplets created"
+  exit 1
+fi
+
+ansible-playbook -i $INVNAME playbooks/initial.yml
+ansible-playbook -i $INVNAME playbooks/deploy.yml
+ansible-playbook -i $INVNAME playbooks/do-shutdown-servers.yml
